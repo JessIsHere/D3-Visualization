d217d9ca4103267b24982c09daef04230f2b4b4d
jessica kim <J_Kim@users.noreply.github.com>
2020-02-09 19:04:18 -0600

    working through sqrtScale

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,36 +1,80 @@
-// ** Unfortunatley they want us to use D3 because they hate us and think plotly is a luxury
 
-// @TODO: YOUR CODE HERE!
+// Create 2 variables defining SVG area dimensions
+
+var svgWidth = 960;
+var svgHeight = 500; 
+
+  console.log(svgWidth);
+  console.log(svgHeight);
+
+
+
+// Create a variable defining the chart's margins as an object
+
+var margin = {
+  top: 60,
+  right: 60,
+  bottom: 60,
+  left: 60,
+};
+  console.log(margin)
+
+
+
+// Create 2 variables to define the dimensions of the chart area
+
+var chartWidth = svgWidth - margin.left - margin.right;
+                // 960 - 60 - 60
+var chartHeight = svgHeight - margin.top - margin.bottom
+                // 500 - 60 - 60
+  console.log(chartHeight);
+  console.log(chartWidth);
+
+
+
+// Create a variable to store svg information - select the body of your html
+
+var svg = d3.select('body')
+
+  // add (append) the SVG area to it - define class
+
+  .append('svg')
+
+  // Set dimensions of svg - define class and call variables storing dimensions
+
+  .attr('width', svgWidth)
+  .attr('height', svgHeight);
+
+  console.log(svg);
+
+
+
+// Create variable for chart that adds(appends) an area to group elements
+
+var chartGroup = svg.append('g')
+
+  // move chart to within margin object
+
+  .attr('transform', `translate(${margin.left}, ${margin.top})`);
+  console.log(chartGroup)
+
+
+// Load csv data
 d3.csv('assets/data/data.csv').then(function(myData) {
 
-  myData.forEach(function(data) {
-    data.healthcare = +data.healthcare;
-    data.poverty = +data.poverty;
-    console.log(myData)
+  // create temporary function to cast string values to int 
+  myData.forEach(function(castInt) {
+    castInt.healthcare = +castInt.healthcare;
+    castInt.poverty = +castInt.poverty;
+    castInt.smokesLow = +castInt.smokesLow;
+    castInt.smokesHigh = +castInt.smokesHigh;
   });
+  console.log(myData)
+
+  // scale data - Using sqrtScale (a specialize case of the powerscale useful for sizing circles by area vs radius)
+  // 
+  var sqrtScale = d3.scaleSqrt()
+    .domain([0, d3.max(myData), castInt => castInt(healthcare)])
+    .range([chartHeight, 0]);
+
 });
-
-// I want to create a new object with the data casted correctly (string vs int)
-
-// Then I need to pull two values to compare ex: healthcare and poverty
-
-// When I pull the values - I also need the associated state abbreviations
-
-
-
-
-// Set Up Chart
-
-// Create SVG Wrapper
-
-// Parse Data
-
-// Format Data
-
-// Create Scales
-
-// Create Axis
-
-// Append Axis to chartGroup
-
-// When I create the scatterplot - I need to somehow be able to have the state abbreveations on the dots. 
