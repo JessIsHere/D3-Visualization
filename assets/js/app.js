cc496dce92b48e25933ec649e3d295d1c7998bfc
jessica kim <J_Kim@users.noreply.github.com>
2020-02-11 22:40:32 -0600

    final commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,11 +6,13 @@ function makeResponsive() {
   // Load csv data
 d3.csv('assets/data/data.csv').then(function(myData) {
 
-  var body = d3.select("body").selectAll('svg');
+  
+  var body = d3.select("#scatter").selectAll('svg');
+  
 
   // clear svg is not empty
   if (!body.empty()) {
-    body.ref();
+    body.remove();
   }
 var svgWidth = window.innerWidth;
 var svgHeight = window.innerHeight;
@@ -32,7 +34,7 @@ var h = svgHeight - margin.top - margin.bottom;
 
 // Create a variable to store svg information - select the body of your html
 
-var svg = d3.selectAll('body')
+var svg = d3.selectAll('#scatter')
 
   // add (append) the SVG area to it - define class
 
@@ -42,29 +44,18 @@ var svg = d3.selectAll('body')
 
   .attr('width', svgWidth)
   .attr('height', svgHeight)
-  .attr("font-size", 5)
-  .attr("font-family", "sans-serif")
-  .attr("text-anchor", "middle")
-  .attr('fill', 'white')
-;
+  
+
 // Create variable for chart that adds(appends) an area to group elements
 
-var g = svg.append('g')
-
-  // f chart to within margin object
-
+var g = svg
+  .append('g')
   .attr('transform', `translate(${margin.left}, ${margin.top})`);
-
+ 
   // create temporary function to cast string values to int 
   myData.forEach(function(castInt) {
-    // var valueKey = myData.columns
-    // console.log(valueKey)
-    // var columns = true
-    //   if(columns==true){
-    //     columns = columns.slice(2,51);
-    //     console.log(myData)
-    //   };
-    castInt.healthcare= +castInt.healthcare;
+   
+    castInt.healthcare = +castInt.healthcare;
     castInt.poverty = +castInt.poverty;
     castInt.smokes = +castInt.smokes;
     castInt.age = +castInt.age;
@@ -72,38 +63,30 @@ var g = svg.append('g')
     castInt.income = +castInt.income;
     
   });
-
+console.log(myData)
   
   // scale data - Using sqrtScale (a specialize case of the powerscale useful for sizing circles by area vs radius)
   
   var xAxis = d3.scaleLinear()
-    .domain([0, d3.max(myData, d => d.healthcare)]).nice()
-    .range([0, w])
+    .domain([d3.min(myData, d => d.healthcare) - 1, d3.max(myData, d => d.healthcare +1)]).nice()
+    .range([0, w]);
 
 
   var yAxis = d3.scaleLinear()
-    .domain([0, d3.max(myData, d => d.poverty)]).nice()
-    .range([h, 0])
+    .domain([d3.min(myData, d => d.poverty) - 1, d3.max(myData, d => d.poverty + 1)]).nice()
+    .range([h, 0]);
   
 
   var xaAxis = d3.scaleLinear()
-    .domain([0, d3.max(myData, d => d.smokes)]).nice()
-    .range([0, w])
+    .domain([d3.min(myData, d => d.smokes) - 1, d3.max(myData, d => d.smokes + 1)]).nice()
+    .range([0, w]);
   
 
   var yaAxis = d3.scaleLinear()
-    .domain([0, d3.max(myData, d => d.age)])
+    .domain([d3.min(myData, d=> d.age) - 1, d3.max(myData, d => d.age + 1 )]).nice()
     .range([h, 0]);
 
-  var xbAxis = d3.scaleLinear()
-    .domain([0, d3.max(myData, d => d.obesity)]).nice()
-    .range([0, w]);
-
-  var ybAxis = d3.scaleLinear()
-    .domain([0, d3.max(myData, d => d.income)])
-    .range([h, 0]);
-  
-  // Step 3: Create axis functions
+  // // Step 3: Create axis functions
 
   var bottomAxis = d3.axisBottom(xAxis);
   var leftAxis = d3.axisLeft(yAxis);
@@ -114,193 +97,123 @@ var g = svg.append('g')
     .attr("stroke-width", '2')
     .selectAll('text')
       .attr("transform", "translate(-10,10)rotate(-45)")
-      .style("text-anchor", "end")
-      .style("font-size", 12)
-      .style("fill", "#182132");
+      .classed('aText', true);
 
   g.append('g')
     .call(leftAxis)
     .attr("stroke-width", '2')
     .selectAll("text")
       .attr("transform", "translate(-10,10)rotate(-45)")
-      .style("text-anchor", "end")
-      .style("font-size", 12)
-      .style("fill", "#182132");
+      .classed('aText', true);
+
+    //   var toolTip = d3.select("circles")
+    //   .append("div")
+    //   .classed("tooltip", true);
+
+    // // Step 2: Create "mouseover" event listener to display tooltip
+    // g.on("mouseover", function(d) {
+    //   toolTip.style("display", "block")
+    //       .html(
+    //         `<strong>${myData(d.healthcare)}<strong><hr>${d.poverty}
+    //     medal(s) won`)
+    //       .style("left", d3.event.pageX + "px")
+    //       .style("top", d3.event.pageY + "px");
+    // })
+    //   // Step 3: Create "mouseout" event listener to hide tooltip
+    //   .on("mouseout", function() {
+    //     toolTip.style("display", "none");
+    //   });
+
+          // ADD .MOUSEOVER
 
   
-    //   g.append('g')
-    //   .attr('transform',`translate(0, ${h})`)
-    //   .call(xaAxis)
-    //   .attr("stroke-width", '2')
-    //   .selectAll('text')
-    //     .attr("transform", "translate(-10,10)rotate(-45)")
-    //     .style("text-anchor", "end")
-    //     .style("font-size", 12)
-    //     .style("fill", "#182132");
+      g.append('g')
+      .attr('transform',`translate(0, ${h})`)
+      .call(xaAxis)
+      .attr("stroke-width", '2')
+      .selectAll('text')
+        .attr("transform", "translate(-10,10)rotate(-45)")
+        .style("text-anchor", "end")
+        .style("font-size", 12)
+        .style("fill", "#182132");
   
-    // g.append('g')
-    //   .call(yaAxis)
-    //   .attr("stroke-width", '2')
-    //   .selectAll("text")
-    //     .attr("transform", "translate(-10,10)rotate(-45)")
-    //     .style("text-anchor", "end")
-    //     .style("font-size", 12)
-    //     .style("fill", "#182132");
+    g.append('g')
+      .call(yaAxis)
+      .attr("stroke-width", '2')
+      .selectAll("text")
+        .attr("transform", "translate(-10,10)rotate(-45)")
+        .style("text-anchor", "end")
+        .style("font-size", 12)
+        .style("fill", "#182132");
 
-    //     g.append('g')
-    //     .attr('transform',`translate(0, ${h})`)
-    //     .call(xbAxis)
-    //     .attr("stroke-width", '2')
-    //     .selectAll('text')
-    //       .attr("transform", "translate(-10,10)rotate(-45)")
-    //       .style("text-anchor", "end")
-    //       .style("font-size", 12)
-    //       .style("fill", "#182132");
-    
-    //   g.append('g')
-    //     .call(ybAxis)
-    //     .attr("stroke-width", '2')
-    //     .selectAll("text")
-    //       .attr("transform", "translate(-10,10)rotate(-45)")
-    //       .style("text-anchor", "end")
-    //       .style("font-size", 12)
-    //       .style("fill", "#182132");
 
-    var tooltip = d3.select("body").append("div")
-    .attr("class", "tooltip")
-    .style("opacity", 0);
-
-    g.append("g")
-    .selectAll('dot')
+    g.selectAll('circle')
     .data(myData)
     .enter()
     .append('circle')
-    .attr('r', 7)
+    .attr('r', 8)
     .attr('cx', f => xAxis(f.healthcare))
     .attr('cy', f => yAxis(f.poverty))
-    .attr('fill', '#d52f4c')
-    .attr("stroke-width", "1")
-    .attr("stroke", "#ffbb00")
-    .on("mouseover", function(d) {
-        tooltip.transition()
-             .duration(200)
-             .style("opacity", .9);
-        tooltip.html(d["healthcare"] + "<br/> (" + xAxis(d) 
-        + ", " + yAxis(d) + ")")
-             .style("left", (d3.event.pageX + 5) + "px")
-             .style("top", (d3.event.pageY - 28) + "px");
-    })
-    .on("mouseout", function(d) {
-        tooltip.transition()
-             .duration(500)
-             .style("opacity", 0);
-    });
-;
-    
+    .classed('stateCircle', true);
 
-    g.append('g')
-    .selectAll('dot')
+    g.selectAll('circles')
+    .data(myData)
+    .enter()
+    .append('text')
+    .text(d => d.abbr)
+    .attr("x", d => xAxis(d.healthcare))
+    .attr("y", d => yAxis(d.poverty))
+    .classed('stateText', true);
+
+    g.selectAll('dots')
     .data(myData)
     .enter()
     .append('circle')
-    .attr('r', 7)
+    .attr('r', 8)
     .attr('cx', f => xaAxis(f.smokes))
     .attr('cy', f => yaAxis(f.age))
-    .attr('fill', '#69c0b8')
-    .attr("stroke-width", "1")
-    .attr("stroke", "#182132");
+    .classed('stateCircle', true);
 
-    g.append('g')
-    .selectAll('dot')
+    g.selectAll('circles')
     .data(myData)
     .enter()
-    .append('circle')
-    .attr('r', 7)
-    .attr('cx', f => xbAxis(f.obesity))
-    .attr('cy', f => ybAxis(f.income))
-    .attr('fill', 'black')
-    .attr("stroke-width", "1")
-    .attr("stroke", "#182132");
+    .append('text')
+    .text(d => d.abbr)
+    .attr("x", d => xaAxis(d.smokes))
+    .attr("y", d => yaAxis(d.age))
+    .classed('stateText', true);
     
     
-     
-
-    // adding abbreviations to circles - cant fully figure it out
-            g.selectAll("dots")
-              .data(myData)
-              .enter()
-              .append("text")
-              // Add your code below this line
-              .attr("x", d => xAxis(d.healthcare))
-              .attr("y", d => yAxis(d.poverty))
-              .text(d => d.abbr);  
-
-              g.selectAll("dots")
-              .data(myData)
-              .enter()
-              .append("text")
-              // Add your code below this line
-              .attr("x", d => xaAxis(d.smokes))
-              .attr("y", d => yaAxis(d.age))
-              .text(d => d.abbr); 
-
-              g.selectAll("dots")
-              .data(myData)
-              .enter()
-              .append("text")
-              // Add your code below this line
-              .attr("x", d => xbAxis(d.obesity))
-              .attr("y", d => ybAxis(d.income))
-              .text(d => d.abbr); 
-              
-              
-
-// Adding axis titles - For some reason they aren't showing but I know the code works - They exist in the html
-
 
               g.append("text")
-              .attr("transform", "rotate(-90)")
-              .attr("text-anchor", "middle")  
-              .style("font-size", "16px") 
-              .style("text-decoration", "underline")
-              .attr("fill", "black")  
-              .attr("dy", "1em")
-              .attr("class", "axisText")
+              .classed('yrotate', true)
               .attr("y", 15 - margin.left + 50)
               .attr("x", 0 - (h / 2))
-              .text("Healthcare")
+              .text("Poverty");
               
               
               g.append('text')
-              .attr("transform", "rotate(-90)")
-              .attr("text-anchor", "middle")  
-              .style("font-size", "16px") 
-              .style("text-decoration", "underline")
-              .attr("fill", "black")  
-              .attr("dy", "1em")
-              .attr("class", "axisText")
-              .attr("y", 15 - margin.left + 25)
-              .attr("x", 0 - (h / 2))
-              .text("Poverty")
+              .classed('yrotate', true)
+              .attr("y", 20 - margin.left + 25)
+              .attr("x", 10 - (h / 2))  
+              .text("Age");
 
 
               g.append("text")
-              .attr("transform", `translate(${w / 2}, ${h + margin.bottom/3})`)
-              .attr("text-anchor", "middle")  
-              .style("font-size", "16px") 
-              .style("text-decoration", "underline")
-              .attr("fill", "black")  
-              .attr("class", "axisText")
-              .text("Age");
-
-              var ytitles = g.append("text")
               .attr("transform", `translate(${w / 2}, ${h + margin.bottom/2})`)
               .attr("text-anchor", "middle")  
-              .style("font-size", "16px") 
-              .style("text-decoration", "underline")
-              .attr("fill", "black")  
-              .attr("class", "axisText")
-              .text("Smokes")
+              .style("font-size", "16px")
+              .attr("fill", "darkslategrey")  
+              .classed("aText", true)
+              .text("Healthcare");
+
+              g.append("text")
+              .attr("transform", `translate(${w / 2}, ${h + margin.bottom/1.5})`)
+              .attr("text-anchor", "middle")  
+              .style("font-size", "16px")
+              .attr("fill", "darkslategrey")  
+              .classed('aText', true)
+              .text("Smokes");
 
     })
     
@@ -315,100 +228,3 @@ makeResponsive();
 
 // // When the browser window is resized, makeResponsive() is called.
 d3.select(window).on("resize", makeResponsive);
-
-    
-
-    // function circleColour(d){
-    //   if(d.healthcare = 'healthcare'){
-    //     return "blue";
-    //   } else {
-    //     return "pink";
-    //   }
-    // }
-
-
-    // trying to create loop for casting integers
-
-            // myData.forEach(function(castInt) {
-            //   var valueKey = myData.columns
-            //   console.log(valueKey)
-            //   var columns = true
-            //     if(columns==true){
-            //       columns = columns.slice(2,51);
-            //       console.log(myData)
-            //     };
-            // };
-
-      
-    // want to create variables to store scatterplot formatting
-            // var dots = g.selectAll('dot')
-            // .data(myData)
-            // .enter()
-            // .append('circle')
-            // .attr('r', 15)
-            // .attr('cx', f => xAxis(f.healthcare))
-            // .attr('cy', f => yAxis(f.poverty))
-            // .attr('fill', '#d52f4c')
-            // .attr("stroke-width", "1")
-            // .attr("stroke", "#ffbb00")
-            // .on("mouseover", function() {
-            //   d3.select(this)
-            //     .transition()
-            //     .duration(100)
-            //     .attr("r", 20)
-            //     .attr("fill", "#ffbb00");
-            //   })
-            //   .on("mouseout", function() {
-            //     d3.select(this)
-            //       .transition()
-            //       .attr("r", 15)
-            //       .duration(100)
-            //       .attr("fill", "#d52f4c");
-            //   });
-            
-
-            // g.selectAll('dot')
-            // .data(myData)
-            // .enter()
-            // .append('circle')
-            // .attr('r', 15)
-            // .attr('cx', f => xxAxis(f.smokes))
-            // .attr('cy', f => yyAxis(f.age))
-            // .attr('fill', '#69c0b8')
-            // .attr("stroke-width", "1")
-            // .attr("stroke", "#182132")
-            // .on("mouseover", function() {
-            //   d3.select(this)
-            //     .transition()
-            //     .duration(100)
-            //     .attr("r", 20)
-            //     .attr("fill", "#182132");
-            //   })
-            //   .on("mouseout", function() {
-            //     d3.select(this)
-            //       .transition()
-            //       .attr("r", 15)
-            //       .duration(100)
-            //       .attr("fill", "#69c0b8");
-            //   });
-
-
-            // // adding abbreviations to circles - cant fully figure it out
-            //         g.selectAll("dot")
-            //           .data(myData)
-            //           .enter()
-            //           .append("text")
-            //           // Add your code below this line
-            //           .attr("x", d => xAxis(d.healthcare))
-            //           .attr("y", d => yAxis(d.poverty))
-            //           .text(d => d.abbr);   
-
-            //           g.selectAll("dot")
-            //           .data(myData)
-            //           .enter()
-            //           .append("text")
-            //           // Add your code below this line
-            //           .attr("x", d => xxAxis(d.smokes))
-            //           .attr("y", d => yyAxis(d.age))
-            //           .text(d => d.abbr); 
-
